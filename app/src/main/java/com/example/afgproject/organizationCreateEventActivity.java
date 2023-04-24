1070f48e6a1b89b4461f7c94f607b8b4147c4322
Emily <ewang1@wpi.edu>
2023-04-24 08:12:50 -0400

    Added navBar to organization event, organization profile, volunteer search, and main.

--- app/src/main/java/com/example/afgproject/organizationCreateEventActivity.java
+++ app/src/main/java/com/example/afgproject/organizationCreateEventActivity.java
@@ -1,12 +1,32 @@
 package com.example.afgproject;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+//nav imports
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.navigation.NavigationView;
+//nav imports
 public class organizationCreateEventActivity extends AppCompatActivity {
+
+    //nav
+    DrawerLayout eventDrawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
+    // /nav
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -66,6 +86,83 @@ public class organizationCreateEventActivity extends AppCompatActivity {
         ArrayAdapter<CharSequence> eventTypeAdapter =ArrayAdapter.createFromResource(this, R.array.eventTypeDropdown, android.R.layout.simple_spinner_item);
        eventTypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         eventTypeDropdownSpinner.setAdapter(eventTypeAdapter);
+
+        //upload button attempt
+        //below video uploads file to firebase storage?
+        //https://www.youtube.com/watch?v=j11O6DvkePg
+        // /upload button attempt
+
+
+
+//nav
+        setUpToolbar();
+        navigationView = (NavigationView) findViewById(R.id.navigation_menu);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId())
+                {
+                    case  R.id.nav_home:
+
+                        Intent homeIntent = new Intent(organizationCreateEventActivity.this, MainActivity.class); //changed both o.c.e.a's from main activity
+                        startActivity(homeIntent);
+                        break;
+
+                    case  R.id.nav_profileDrawer:
+
+                        Intent profileIntent = new Intent(organizationCreateEventActivity.this, organizationProfile.class); //changed both o.c.e.a's from main activity
+                        startActivity(profileIntent);
+                        break;
+
+
+                    case  R.id.nav_eventDrawer:
+
+                        Intent eventIntent = new Intent(organizationCreateEventActivity.this, organizationCreateEventActivity.class); //changed both o.c.e.a's from main activity
+                        startActivity(eventIntent);
+                        break;
+
+                    case  R.id.nav_volunteerSearchDrawer:
+
+                        Intent searchIntent = new Intent(organizationCreateEventActivity.this, volunteerSearch.class);
+                        startActivity(searchIntent);
+                        break;
+
+
+//Paste your privacy policy link
+
+//                    case  R.id.nav_Policy:{
+//
+//                        Intent browserIntent  = new Intent(Intent.ACTION_VIEW , Uri.parse(""));
+//                        startActivity(browserIntent);
+//
+//                    }
+                    //       break;
+                    case  R.id.nav_share:{
+
+                        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                        sharingIntent.setType("text/plain");
+                        String shareBody =  "http://play.google.com/store/apps/detail?id=" + getPackageName();
+                        String shareSub = "Try now";
+                        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
+                        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                        startActivity(Intent.createChooser(sharingIntent, "Share using"));
+
+                    }
+                    break;
+                }
+                return false;
+            }
+        });
+    }
+    public void setUpToolbar() {
+        eventDrawerLayout = findViewById(R.id.eventDrawerLayout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, eventDrawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        eventDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+// /nav
     }
 
 
--- app/src/main/java/com/example/afgproject/organizationProfile.java
+++ app/src/main/java/com/example/afgproject/organizationProfile.java
@@ -1,17 +1,100 @@
 package com.example.afgproject;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class organizationProfile extends AppCompatActivity {
+
+    //nav
+    DrawerLayout profileDrawerLayout;
+    ActionBarDrawerToggle actionBarDrawerToggle;
+    NavigationView navigationView;
+    // /nav
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_organization_profile);
+
+
+//nav
+        setUpToolbar();
+        navigationView = (NavigationView) findViewById(R.id.navigation_menu);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId())
+                {
+                    case  R.id.nav_home:
+
+                        Intent homeIntent = new Intent(organizationProfile.this, MainActivity.class); //changed both o.c.e.a's from main activity
+                        startActivity(homeIntent);
+                        break;
+
+                    case  R.id.nav_profileDrawer:
+
+                        Intent profileIntent = new Intent(organizationProfile.this, organizationProfile.class); //changed both o.c.e.a's from main activity
+                        startActivity(profileIntent);
+                        break;
+
+
+                    case  R.id.nav_eventDrawer:
+
+                        Intent eventIntent = new Intent(organizationProfile.this, organizationCreateEventActivity.class); //changed both o.c.e.a's from main activity
+                        startActivity(eventIntent);
+                        break;
+
+                    case  R.id.nav_volunteerSearchDrawer:
+
+                        Intent searchIntent = new Intent(organizationProfile.this, volunteerSearch.class);
+                        startActivity(searchIntent);
+                        break;
+
+//Paste your privacy policy link
+
+//                    case  R.id.nav_Policy:{
+//
+//                        Intent browserIntent  = new Intent(Intent.ACTION_VIEW , Uri.parse(""));
+//                        startActivity(browserIntent);
+//
+//                    }
+                    //       break;
+                    case  R.id.nav_share:{
+
+                        Intent sharingIntent = new Intent(android.content.Intent.ACTION_SEND);
+                        sharingIntent.setType("text/plain");
+                        String shareBody =  "http://play.google.com/store/apps/detail?id=" + getPackageName();
+                        String shareSub = "Try now";
+                        sharingIntent.putExtra(android.content.Intent.EXTRA_SUBJECT, shareSub);
+                        sharingIntent.putExtra(android.content.Intent.EXTRA_TEXT, shareBody);
+                        startActivity(Intent.createChooser(sharingIntent, "Share using"));
+
+                    }
+                    break;
+                }
+                return false;
+            }
+        });
+    }
+    public void setUpToolbar() {
+        profileDrawerLayout = findViewById(R.id.profileDrawerLayout);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        actionBarDrawerToggle = new ActionBarDrawerToggle(this, profileDrawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        profileDrawerLayout.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+
+// /nav
 
 }}
