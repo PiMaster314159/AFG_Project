8ef428abc7ede0ad9c22861567a1fe3b629ec50a
Kakese24 <128811048+Kakese24@users.noreply.github.com>
2023-05-14 23:32:55 -0400

    BY KWEKU FOR DONOVAN created a UI and firebase activity for adding acitivites (store in firebase) THIS IS FOR DONOVAN

--- app/src/main/java/com/example/afgproject/MainActivity.java
+++ app/src/main/java/com/example/afgproject/MainActivity.java
@@ -1,45 +1,14 @@
 package com.example.afgproject;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentSender;
 import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.location.LocationManager;
-//import android.location.LocationRequest;
-import android.os.Build;
 import android.os.Bundle;
-import android.os.Looper;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 import android.view.View.OnClickListener;
-import android.widget.Toast;
+import android.widget.Button;
 
-//import com.google.android.gms.location.LocationAvailability;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.common.api.ResolvableApiException;
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationResult;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.LocationSettingsResponse;
-import com.google.android.gms.location.LocationSettingsStatusCodes;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.android.material.navigation.NavigationView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -61,7 +30,6 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println(getSharedPreferences((getString(R.string.user_preferences_map)), MODE_PRIVATE).getBoolean("onStart", false));
                 SharedPreferences sharedPref = getSharedPreferences((getString(R.string.user_preferences_map)), MODE_PRIVATE);
                 sharedPref.edit().putBoolean("onStart", true).apply();
-
                 setContentView(R.layout.activity_main);
 
                 volunteerButton = findViewById(R.id.volunteer_button);
